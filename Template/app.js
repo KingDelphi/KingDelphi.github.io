b0acbcbd9000e7e7f0c1d8dcc2003258f51aedb5
KingDelphi <124750383+KingDelphi@users.noreply.github.com>
2023-09-24 08:32:11 -0500

    Version 1.0.12
    Version 1.0.12

--- Template/app.js
+++ Template/app.js
@@ -2,11 +2,11 @@
     
     //UNITY STUFF
     var buildUrl = "Build";
-    var loaderUrl = buildUrl + "/1.0.11 WebGL Build.loader.js";
+    var loaderUrl = buildUrl + "/1.0.12 WebGL Build.loader.js";
     var config = {
-        dataUrl: buildUrl + "/1.0.11 WebGL Build.data",
-        frameworkUrl: buildUrl + "/1.0.11 WebGL Build.framework.js",
-        codeUrl: buildUrl + "/1.0.11 WebGL Build.wasm",
+        dataUrl: buildUrl + "/1.0.12 WebGL Build.data",
+        frameworkUrl: buildUrl + "/1.0.12 WebGL Build.framework.js",
+        codeUrl: buildUrl + "/1.0.12 WebGL Build.wasm",
         streamingAssetsUrl: "StreamingAssets",
         companyName: "Delphium",
         productName: "Delphic Chess",
